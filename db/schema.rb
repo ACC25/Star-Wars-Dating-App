8b4681a487c51be6628e863c5aedb36aeb0dd5a6
Carl Richmond <richmond.carl@outlook.com>
2017-05-23 20:54:54 -0600

    changed db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170523214113) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.text "username"
     t.datetime "created_at", null: false
