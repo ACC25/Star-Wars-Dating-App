c2431abeccc84fcb650f69640466e875bdb9778f
Carl Richmond <richmond.carl@outlook.com>
2017-05-25 08:59:52 -0600

    work on form sending correct information to correct controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,8 +8,12 @@ class SearchesController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = current_user
+    @gender = params.keys[0]
+    @smartness = params.keys[1]
+    @species = params.keys[2]
+    @climate = params.keys[3]
+    binding.pry
     @species = Race.find(params["search"]["name"].to_i).name
   end
 end
